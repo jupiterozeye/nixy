07a70d2094e6beae9f4624406a2511ff8a48d2e0
jupiterozeye <ozeye@tutamail.com>
2026-01-05 14:55:36 +0100

    test

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
 
   outputs = inputs @ {nixpkgs, ...}: {
     nixosConfigurations = {
-      nixy =
+      worklaptop =
         # CHANGEME: This should match the 'hostname' in your variables.nix file
         nixpkgs.lib.nixosSystem {
           modules = [
@@ -47,10 +47,9 @@
                 inherit inputs;
               };
             }
-            inputs.nixos-hardware.nixosModules.omen-16-n0005ne # CHANGEME: check https://github.com/NixOS/nixos-hardware
             inputs.home-manager.nixosModules.home-manager
             inputs.stylix.nixosModules.stylix
-            ./hosts/laptop/configuration.nix # CHANGEME: change the path to match your host folder
+            ./hosts/worklaptop/configuration.nix # CHANGEME: change the path to match your host folder
           ];
         };
       # Jack is my server
---
+++ hosts/worklaptop/variables.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    # Choose your theme here:
+    ../../themes/rose-pine.nix
+  ];
+
+  config.var = {
+    hostname = "worklaptop";
+    username = "jupi";
+    configDirectory =
+      "/home/"
+      + config.var.username
+      + "/.config/nixos"; # The path of the nixos configuration directory
+
+    keyboardLayout = "us";
+
+    location = "Paris";
+    timeZone = "Europe/Paris";
+    defaultLocale = "en_US.UTF-8";
+    extraLocale = "fr_FR.UTF-8";
+
+    git = {
+      username = "jupiterozeye";
+      email = "ozeye@tutamail.com";
+    };
+
+    autoUpgrade = false;
+    autoGarbageCollector = true;
+  };
+
+  # DON'T TOUCH THIS
+  options = {
+    var = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+    };
+  };
+}
