bb456ab730b3b931c5bc24c0ee7f644bd0529634
jupiterozeye <ozeye@tutamail.com>
2026-01-06 08:49:04 +0100

    added missing files

---
+++ home/programs/mullvad/default.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    mullvad-vpn
+    mullvad
+  ];
+
+  # Auto-statrt Proton VPN
+
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "mullvad-vpn-app --start-minimized &"
+  ];
+}
