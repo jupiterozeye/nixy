cfc0944fce19e1a65a0a993f2a7991bde3eaf10b
jupiterozeye <ozeye@tutamail.com>
2026-02-01 19:13:12 +0100

    optimized bindings

--- home/system/niri/default.nix
+++ home/system/niri/default.nix
@@ -64,6 +64,7 @@ in {
       "Mod+Return".action = spawn "ghostty";
       "Mod+Space".action = spawn "fuzzel";
       "Super+Alt+L".action = spawn "swaylock";
+      "Super+B".acion = spawn "Brave";
 
       # Window management
       "Mod+Q".action = close-window;
@@ -110,13 +111,35 @@ in {
       "Mod+Ctrl+Shift+L".action = move-column-to-monitor-right;
       "Mod+Ctrl+Shift+Right".action = move-column-to-monitor-right;
 
-      # Workspace switching
+      # Workspace switching (numbered)
+      "Mod+1".action.focus-workspace = 1;
+      "Mod+2".action.focus-workspace = 2;
+      "Mod+3".action.focus-workspace = 3;
+      "Mod+4".action.focus-workspace = 4;
+      "Mod+5".action.focus-workspace = 5;
+      "Mod+6".action.focus-workspace = 6;
+      "Mod+7".action.focus-workspace = 7;
+      "Mod+8".action.focus-workspace = 8;
+      "Mod+9".action.focus-workspace = 9;
+
+      # Workspace switching (relative)
       "Mod+U".action = focus-workspace-down;
       "Mod+Page_Down".action = focus-workspace-down;
       "Mod+I".action = focus-workspace-up;
       "Mod+Page_Up".action = focus-workspace-up;
 
-      # Move column to workspace
+      # Move window to workspace (numbered)
+      "Mod+Shift+1".action.move-column-to-workspace = 1;
+      "Mod+Shift+2".action.move-column-to-workspace = 2;
+      "Mod+Shift+3".action.move-column-to-workspace = 3;
+      "Mod+Shift+4".action.move-column-to-workspace = 4;
+      "Mod+Shift+5".action.move-column-to-workspace = 5;
+      "Mod+Shift+6".action.move-column-to-workspace = 6;
+      "Mod+Shift+7".action.move-column-to-workspace = 7;
+      "Mod+Shift+8".action.move-column-to-workspace = 8;
+      "Mod+Shift+9".action.move-column-to-workspace = 9;
+
+      # Move column to workspace (relative)
       "Mod+Ctrl+U".action = move-column-to-workspace-down;
       "Mod+Ctrl+Page_Down".action = move-column-to-workspace-down;
       "Mod+Ctrl+I".action = move-column-to-workspace-up;
